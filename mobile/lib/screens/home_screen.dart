406780d2587d9f5aca682a0fcb56bf6fe8ded5df
agbeviade <agbeviade@users.noreply.github.com>
2026-04-25 11:43:28 +0000

    fix: import dart:typed_data + force NDK 27

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:image_picker/image_picker.dart';
